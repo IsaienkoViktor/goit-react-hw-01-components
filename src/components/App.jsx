cc78a0ad87627d9085f1f61fa67eb90a52042a24
IsaienkoViktor <isaenkov1986@gmail.com>
2023-06-19 18:21:11 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,11 @@
 import { Profile } from './Profile/Profile.js';
+import { Statistics } from './Statistics/Statistics.js';
+import { FriendList } from './FriendList/FriendList.js';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory.js';
+
+import transactions from '../data/transactions.json';
+import friends from '../data/friends.json';
+import data from 'data/data.json';
 import user from 'data/user.json';
 
 export const App = () => {
@@ -11,6 +18,9 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />;
+      <TransactionHistory items={transactions} />;
     </>
   );
 };
